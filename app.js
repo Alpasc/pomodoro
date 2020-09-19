20137ba30488617ae965422eea24be3044401387
Alpasc <alinepascal@gmail.com>
2020-09-19 11:35:13 +0200

    mise en place du timer et des actions ok

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
-const affichageTravail = document.querySelector('.affichageT');
-const affichagePause = document.querySelector('.affichageP');
-const btnGo = document.querySelector('b1');
-const btnPause = document.querySelector('b2');
-const btnReset = document.querySelector('b3');
-const cycles = document.querySelector('h2');
+const affichageTravail = document.querySelector(".affichageT");
+const affichagePause = document.querySelector(".affichageP");
+const btnGo = document.querySelector(".b1");
+const btnPause = document.querySelector(".b2");
+const btnReset = document.querySelector(".b3");
+const cycles = document.querySelector("h2");
 
-let tempsInitial = 1800;
-let tempsDeRepos = 300;
+let tempsInitial = 5;
+let tempsDeRepos = 3;
 let pause = false;
 let nbDeCycles = 0;
 cycles.innerText = `Nombre de cycles : ${nbDeCycles}`;
 
-affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
-affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
+affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
+  tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
+}`;
+affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${
+  tempsDeRepos % 60 < 10 ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60
+}`;
+
+btnGo.addEventListener("click", () => {
+  tempsInitial--;
+  affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
+    tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
+  }`;
+
+  let timer = setInterval(() => {
+    if (tempsInitial > 0) {
+      tempsInitial--;
+      affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
+        tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
+      }`;
+    } else if (tempsInitial === 0 && tempsDeRepos > 0) {
+      tempsDeRepos--;
+      affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${
+        tempsDeRepos % 60 < 10 ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60
+      }`;
+    } else if (tempsDeRepos === 0 && tempsInitial === 0) {
+      tempsInitial = 5;
+      tempsDeRepos = 3;
+      nbDeCycles++;
+      cycles.innerText = `Nombre de cycles : ${nbDeCycles}`;
+      affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
+        tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
+      }`;
+      affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${
+        tempsDeRepos % 60 < 10 ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60
+      }`;
+      tempsInitial--;
+      affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
+        tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
+      }`;
+    }
+  }, 1000);
+});
